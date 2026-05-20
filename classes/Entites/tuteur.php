9e17358cd78d7f892cd28201dc309d0f7dfd9215
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-20 16:59:22 +0100

    Methodes marquer resolue et laisserAvis

--- classes/Entites/tuteur.php
+++ classes/Entites/tuteur.php
@@ -3,7 +3,13 @@ require_once"User.php";
 
 class Apprenant extends User{
     public function creerDemande(string $titre, string $description, int $competenceId){
-        
+
+
+    }
+    public function marquerResolue(int $demandeId):void{
+
+    }
+    public function laisserAvis(int $demandeId, int $note):void{
 
     }
 }

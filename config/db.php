6a656697f85ab78ebc530dd4d80d680780c8316e
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-20 15:54:22 +0100

    Faut de nommage

--- config/db.php
+++ config/db.php
@@ -4,13 +4,13 @@
 class DB {
     private static $connection= null;
 
-    public static function getConnction () :PDO{
+    public static function getConnection () :PDO{
 
 
         if(self::$connection==null){
 
             //crer la connection 
-            self::$connection= new PDO("myqsl:host=localhost;dbname=peerenaa","root","");
+            self::$connection= new PDO("mysql:host=localhost;dbname=peerenaa","root","");
             //cofigurer le PDO en=n cas d'erreur
             self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }

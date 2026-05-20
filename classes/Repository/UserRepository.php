99697090a3091ba1dc67fe7197bf0977ea5acc1c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-20 17:44:34 +0100

    faire une requette + la sécurité

--- classes/Repository/UserRepository.php
+++ classes/Repository/UserRepository.php
@@ -6,4 +6,16 @@ class UserRepository {
     public function __construct(PDO $db) {
         $this->db = $db;
     }
+
+      public function findById(int $id): ?User {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$data) return null;
+
+        return null;
+    }
 }    

163319b5f65f8d539866ac0d4f5af92c534ea4b6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-20 16:11:15 +0100

    Une requette pour récupérer les id des users et la sécurité contre injection sql

--- classes/Entites/User.php
+++ classes/Entites/User.php
@@ -6,9 +6,17 @@ abstract class user{
    protected $db;
 
    public function __construct()
-     {
-       $this->db=DB::getConnction;
-   
-     }
+       {
+       $this->db=DB::getConnection;
+     
+       }
+   public function getById($id){
+
+      $sql=" Select* from user where id=?";
+      $stmt=$this->db->prepare($sql);
+      $stmt->execute([$id]);
+      return $stmt->fetch(PDO::FETCH_ASSOC );
+
+   }   
 
 }

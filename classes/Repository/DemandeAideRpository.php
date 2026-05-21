75105a67791f5b9b8fac9bca4713a4bc650aaf68
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 12:02:46 +0100

    Creation class et propriété de type PDO avec constructeur

--- classes/Repository/DemandeAideRpository.php
+++ classes/Repository/DemandeAideRpository.php
@@ -2,3 +2,12 @@
 declare(strict_types=1);
 require_once'../Entites/DemandAide.php';
 require_once'../Entites/Statut.php';
+
+class DemandeAideRpository{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        throw new \Exception('Not implemented');
+    }
+}

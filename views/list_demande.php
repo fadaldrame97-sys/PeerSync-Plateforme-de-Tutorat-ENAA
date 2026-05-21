61bb988ecf14bc93826deab778bfcfe5512095b8
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 23:05:49 +0100

    stocker toutes les demandes dans un variable et les afficher

--- views/list_demande.php
+++ views/list_demande.php
@@ -7,4 +7,24 @@ require_once "../Entities/Statut.php";
 require_once "../Repository/DemandeAideRepository.php";
 $db = DB::getConnection();
 
-$repository = new DemandeAideRepository($db);
+$repository = new DemandeAideRepository($db); 
+$demandes = $repository->findAll();
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List des demandes</title>
+</head>
+<body>
+    <h1>Liste des demandes</h1>
+    <?php foreach($demandes as $demandes):?>
+    
+    <?php endforeach ?>    
+</body>
+</html>

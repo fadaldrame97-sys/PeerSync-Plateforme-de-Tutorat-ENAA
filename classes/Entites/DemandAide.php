ce0cb8b3c1b581e8a5480faebfb74531b67e9d96
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 10:55:40 +0100

    Correction nommage

--- classes/Entites/DemandAide.php
+++ classes/Entites/DemandAide.php
@@ -1,14 +1,15 @@
 <?php
 declare(strict_types=1);
+require_once "Statut.php";
 
-class DemandAide{
+class DemandeAide{
     private int $id;
     private string $titre;
 
     private string $description;
-    private string $statut;
+    private statut $statut;
     private int $apprenant_id;
-    private int $tuteur_id;
+    private ? int $tuteur_id;
 
     public function __construct(
         int $id,
@@ -26,12 +27,12 @@ class DemandAide{
          $this->apprenant_id=$apprenant_id;
          $this->tuteur_id=$tuteur_id;
      }
-     public function assignTo(int $tuteur_id){
+     public function assignTo(int $tuteur_id):void{
         if ($this->apprenant_id==$tuteur_id){
             throw new Exception("Impossible de s'assigner soit-meme");
         }
         $this->tuteur_id=$tuteur_id;
-        $this->statut="assigned";
+       $this->statut = Statut::RESOLVED;
 
      }
 

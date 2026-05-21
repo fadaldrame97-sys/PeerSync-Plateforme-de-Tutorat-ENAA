7697604ffc3f57228f41fed388ee15015392ffed
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 10:23:11 +0100

    Constructeur argumenté

--- classes/Entites/DemandAide.php
+++ classes/Entites/DemandAide.php
@@ -9,4 +9,16 @@ class DemandAide{
     private string $statut;
     private int $apprenant_id;
     private int $tuteur_id;
+
+    public function __construct(
+        int $id,
+        string $titre,
+        string $description,
+        string $statut,
+        int $apprenant_id,
+        int $tuteur_id,
+
+     ){
+        
+     }
 }

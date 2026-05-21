237a08660638a13c2fe03040a5d854af30a5bd33
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 11:29:17 +0100

    Utiliser le constructeur du User

--- classes/Entites/tuteur.php
+++ classes/Entites/tuteur.php
@@ -1,7 +1,8 @@
 <?php
 require_once"User.php";
 class Tuteur extends User{
-    public function accepterDemande():void{
-        
+    public function accepterDemande(int $id, string $name):void{
+         parent::__construct($id, $name);
+
     }
 }

264bd1582fc6942a18ac161164dbfb902cbe9217
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 16:59:19 +0100

    Importer les fichiers

---
+++ views/nouvelle_demande.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+require_once "../config/db.php";
+
+require_once "../Entities/DemandeAide.php";
+require_once "../Entities/Statut.php";
+
+require_once "../Repository/DemandeAideRepository.php";

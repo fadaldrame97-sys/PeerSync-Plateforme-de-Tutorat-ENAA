4e455f7bd838a4baebea758c4fc1d1c375f0b842
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-21 10:19:10 +0100

    activer typage strict et créer classe DemandeAid avec ces propriétés

---
+++ classes/Entites/DemandAide.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+class DemandAide{
+    private int $id;
+    private string $titre;
+
+    private string $description;
+    private string $statut;
+    private int $apprenant_id;
+    private int $tuteur_id;
+}

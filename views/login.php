f49c68351464ec98ffbd22cc4b4f0011bee419b9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 10:51:51 +0100

    Créer un form et ajouter du style

--- views/login.php
+++ views/login.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Se connecter</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    
+<body  class="bg-gray-100 flex items-center justify-center h-screen">
+
+    <form action="login.php" method="POST" class="bg-white p-8 rounded-2xl shadow-lg w-96"></form>
 </body>
 </html>

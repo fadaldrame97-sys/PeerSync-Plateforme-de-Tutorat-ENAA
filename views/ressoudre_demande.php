1de42a1e3551ff1a219d3b006773d0d267136233
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 09:54:31 +0100

    Créer new obejet de DemandeRepository

--- views/ressoudre_demande.php
+++ views/ressoudre_demande.php
@@ -3,4 +3,5 @@ require_once "../config/db.php";
 require_once "../classes/Repository/DemandeAideRepository.php";
 require_once "../classes/Entities/Statut.php";
 $db= DB::getConnection();
+$repo= new DemandeAideRepository($db);
 

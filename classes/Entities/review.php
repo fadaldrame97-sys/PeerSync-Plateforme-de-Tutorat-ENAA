3580dd68293d1ef38f0dfbf1eeded53e5a3e477f
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 17:38:46 +0100

    Création de la classe Review  avec le constructeur et methode getter pour accerder à ces propriétés privées

---
+++ classes/Entities/review.php
@@ -0,0 +1,72 @@
+ <?php
+class Review {
+     
+    private int $id;
+    private int $demandeAideId;
+    private int $apprenantId;
+    private int $tuteurId;
+    private int $note;
+    private ?string $commentaire;
+
+    public function __construct( 
+        int $id,
+        int $demandeAideId,
+        int $apprenantId,
+        int $tuteurId,
+        int $note,
+        string $commentaire)
+    {
+        $this->id=$id;
+        $this->demandeAideId=$demandeAideId;
+        $this->apprenantId=$apprenantId;
+        $this->note=$note;
+        $this->commentaire=$commentaire;
+
+    }
+
+        public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getDemandeAideId(): int
+    {
+        return $this->demandeAideId;
+    }
+
+    public function getApprenantId(): int
+    {
+        return $this->apprenantId;
+    }
+
+    public function getTuteurId(): int
+    {
+        return $this->tuteurId;
+    }
+
+    public function getNote(): int
+    {
+        return $this->note;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+}
+
+
+
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 

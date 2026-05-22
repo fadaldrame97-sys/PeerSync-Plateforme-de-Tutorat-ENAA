c8d31efd734a9d7d5e0f4ddc71d0966143886a46
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 12:23:50 +0100

    Constructeur pramettré et initialiser les parametrres

--- classes/Entities/competence.php
+++ classes/Entities/competence.php
@@ -2,4 +2,10 @@
 class Competence{
     private int $id;
     private string $nom;
+
+    public function __construct(int $id, string $nom)
+    {
+        $this->id=$id;
+        $this->nom=$nom;
+    }
 }

2b48e330072f26e82d875a348740e3796cb7f6f1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-23 00:42:55 +0100

    Créer les objects

--- views/ajouter_competence_forte.php
+++ views/ajouter_competence_forte.php
@@ -10,3 +10,11 @@ require_once "../classes/Repository/PointFortRepository.php";
 require_once "../classes/Repository/CompetenceRepository.php";
 
 $db = DB::getConnection();
+
+$pointFortRepo = new PointFortRepository($db);
+
+$competenceRepo = new CompetenceRepository($db);
+
+$competences = $competenceRepo->findAll();
+
+$message = "";

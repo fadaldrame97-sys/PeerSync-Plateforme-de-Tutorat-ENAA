a5fe166a226e4d8c9d894210632f7ff883440ba2
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 10:35:50 +0100

    imporert le DemandeAide.php

--- views/ressoudre_demande.php
+++ views/ressoudre_demande.php
@@ -2,6 +2,7 @@
 require_once "../config/db.php";
 require_once "../classes/Repository/DemandeAideRepository.php";
 require_once "../classes/Entities/Statut.php";
+require_once "../classes/Entities/DemandeAide.php";
 $db= DB::getConnection();
 $repo= new DemandeAideRepository($db);
 

97b3fd082954c37c319c255cc2522539ba77bd3f
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 10:28:07 +0100

    créer titreId=1 temporairement avant la création de la SESSION

---
+++ views/accepter_demande.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../config/db.php";
+require_once "../classes/Repository/DemandeAideRepository.php";
+require_once "../classes/Entities/Statut.php";
+$db= DB::getConnection();
+$repo= new DemandeAideRepository($db);
+
+$id=(int) $_GET['id'];
+
+$tuteurID=1;
+//Créer un objet DemandeAide avec titre vide et description vide
+$demande= new DemandeAide($id,"","",Statut::RESOLUE,0,null);
+
+$repo->update($demande);
+
+header("Location: list_demande.php"); 

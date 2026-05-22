a1378fa1c55d8b603e87ff018785792372047ad7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-22 10:58:45 +0100

    Ajouter input passeword

--- views/login.php
+++ views/login.php
@@ -17,6 +17,19 @@
                   type="email"name="email"required class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
         </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 text-gray-700">
+                Mot de passe
+            </label>
+
+            <input
+                type="password"
+                name="password"
+                required
+                class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+        </div>
     </form>
 </body>
 </html>

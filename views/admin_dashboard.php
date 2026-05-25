31aa71b5636c783f93e4a642b0c24ed1585472ab
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-25 12:34:32 +0100

    Je prépare l'affichage du Dashboard dans l'interface

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -27,3 +27,16 @@ $totalTuteurs = $repo->NombreTuteurs();
 $totalApprenants = $repo->NombreApprenants();
 
 $topTuteurs = $repo->topTuteurs();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+</head>
+<body>
+    
+</body>
+</html>

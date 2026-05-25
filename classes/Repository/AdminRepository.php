569b6a53fae17910498e46fcec689ae24f5fe5eb
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-25 11:59:44 +0100

    Fonction NombreTuteurs avec requette SQL pour récupérer le nombre

--- classes/Repository/AdminRepository.php
+++ classes/Repository/AdminRepository.php
@@ -28,4 +28,15 @@ class AdminRepository
 
         return (int)$this->db->query($sql)->fetchColumn();
     }
+        // Nombre tuteurs
+    public function countTuteurs(): int
+    {
+        $sql = "
+            SELECT COUNT(*)
+            FROM users
+            WHERE role = 'tuteur'
+        ";
+
+        return (int)$this->db->query($sql)->fetchColumn();
+    }
 }

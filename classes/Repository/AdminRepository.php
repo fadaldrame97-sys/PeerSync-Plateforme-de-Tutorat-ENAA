7f3e37837c544c9da0758d6e4532d0c324298ceb
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-25 11:54:07 +0100

    J'ai créé la classe et une propriété PDO et une fonctions pour totaliser nombre de demande aide et nombre de demandes resolues

---
+++ classes/Repository/AdminRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+class AdminRepository
+{
+    private PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    // Nombre total demandes
+    public function countDemandes(): int
+    {
+        $sql = "SELECT COUNT(*) FROM demandeAide";
+
+        return (int)$this->db->query($sql)->fetchColumn();
+    }
+
+    // Nombre demandes résolues
+    public function countResolved(): int
+    {
+        $sql = "
+            SELECT COUNT(*)
+            FROM demandeAide
+            WHERE statut = 'resolue'
+        ";
+
+        return (int)$this->db->query($sql)->fetchColumn();
+    }
+}

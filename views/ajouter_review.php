20c9db27f4b1be30a411d686c649672964c3ae79
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-25 11:26:02 +0100

    La partie affichage dans l'interface

--- views/ajouter_review.php
+++ views/ajouter_review.php
@@ -34,3 +34,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donner un avis</title>
+</head>
+<body>
+    
+</body>
+</html>
